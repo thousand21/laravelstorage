5e833e3638582e4338c0b10565a5af8342c977e7
unknown <thousand24@outlook.fr>
2021-09-06 14:27:49 +0200

    la

--- database/factories/PersonneFactory.php
+++ database/factories/PersonneFactory.php
@@ -23,6 +23,15 @@ class PersonneFactory extends Factory
     {
         return [
             //
+            "nom"=>$this->faker->lastName(),
+            "prenom"=>$this->faker->firstName('male'|'female'),
+            "age"=>$this->faker->randomBetween($min=18,$max=80),
+            "email"=>$this->faker->email(),
+            "mdp"=>$this->faker->password(),
+            "dateNaissance"=>$this->faker->dateTimeThisCentury(),
+            "photo"=>$this->faker->imageUrl($width = 640, $height = 480),
+            
+            "description"=>$this->faker->realText($maxNbChars = 200, $indexSize = 2)
         ];
     }
 }
